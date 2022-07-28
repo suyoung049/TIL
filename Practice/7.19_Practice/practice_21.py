603ef4b60d64c92f1ee5e4954bd1a95ab36b02d9
suyoung049 <suyoung049@gmail.com>
2022-07-28 22:16:06 +0900

    7.28 백준 문제 풀이 추가

--- Practice/7.19_Practice/practice_21.py
+++ Practice/7.19_Practice/practice_21.py
@@ -5,14 +5,7 @@ i = 0
 while n > 0:
     i = n % 10 
     n //= 10   
-    print(int(i, end =''))
+    print(i, end ='')
 
 # number = 123
 # print(int(str(number)[::-1]))
-
-num = 123
-result = 0
-while num:
-    result %= 10
-    result += num%10
-print(result)
