bb9ba66d7a76c26c05d0379d9c34c42691432390
suyoung049 <suyoung049@gmail.com>
2022-08-24 23:22:39 +0900

    0824 실습풀이 Upload

---
+++ Daily_code_Practice/August/8.24_Practice/1_달팽이.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('1_input.txt', 'r')
+
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    n = int(input())
+    matrix = [[0]*n for _ in range(n)]
+
+    r, c = 0, 0
+    dist = 0
+
+    for i in range(1, n*n + 1):
+        matrix[r][c] = i
+        r += dr[dist]
+        c += dc[dist]
+
+        if r < 0 or c < 0 or r >= n or c >= n or matrix[r][c] != 0:
+            r -= dr[dist]
+            c -= dc[dist]
+
+            dist = (dist+1) % 4
+
+            r += dr[dist]
+            c += dc[dist]
+    
+    for row in matrix:
+        print(*row)
+    print()
