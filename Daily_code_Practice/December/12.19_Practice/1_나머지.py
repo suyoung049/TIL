5bdb069b24493564c414191a494127217086806c
suyoung049 <suyoung049@gmail.com>
2022-12-19 18:44:36 +0900

    Daily code practice upload

---
+++ Daily_code_Practice/December/12.19_Practice/1_나머지.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+sys.stdin = open('1_input.txt', 'r')
+input = sys.stdin.readline
+
+A, B, C = map(int, input().split())
+
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
