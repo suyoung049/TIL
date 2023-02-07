b03f46c1c65bc52d0424fba73eef4558c20672df
suyoung049 <suyoung049@gmail.com>
2023-02-07 23:43:33 +0900

    Daily Coad Upload

---
+++ Daily_code_Practice/2023_ALGO/February/2.7_Practice/무인도.py
@@ -0,0 +1,49 @@
+from collections import deque
+maps = ["X591X","X1X5X","X231X", "1XXX1"]
+
+for i in maps:
+    m = len(i)
+
+n = len(maps)
+check = [[False]*m for _ in range(n)]
+answer = []
+
+
+def pprint(list_):
+    for row in list_:
+        print(row)
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+def bfs(a, b, c):
+    q = deque([(a,b)])
+
+    while q:
+        y, x = q.popleft()
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if 0<= ny < n and 0<= nx < m and not check[ny][nx]:
+                if maps[ny][nx] != 'X':
+                    check[ny][nx] = True
+                    c += int(maps[ny][nx])
+                    q.append((ny,nx))
+    return c
+   
+
+
+
+for j in range(n):
+    for i in range(m):
+        sum_ = 0
+        if maps[j][i] != 'X' and not check[j][i]:
+            check[j][i] = True
+            sum_ += int(maps[j][i])
+            print(bfs(j, i, sum_))
+
+pprint(check)
+
+        
