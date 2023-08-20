f7610a6c5727b46ff95b42e2ed4ce4428da137f7
suyoung049 <suyoung049@gmail.com>
2023-08-20 22:58:47 +0900

    알고리즘 백트래킹

---
+++ Daily_code_Practice/2023_ALGO/August/8.19_Practice/1.NM7.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open("1_input.txt", "r")
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+num_li = list(map(int, input().split()))
+
+num_li.sort()
+answer = []
+def dfs(k, s, tlist):
+    if k == m:
+        answer.append(tlist)
+        return
+    
+    prev = 0
+
+    for i in range(n):
+        if prev != num_li[i]:
+            prev = num_li[i]
+            dfs(k+1, i, tlist + [num_li[i]])
+
+dfs(0, 0, [])
+
+for j in answer:
+    print(*j)
